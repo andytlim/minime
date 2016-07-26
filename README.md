773cee4f8fc697c9df6bd177253cbd790d5fa7aa
Andy Lim <andytlim@gmail.com>
2016-07-25 22:59:27 -0400

    Updated minime.json file format. Updated code to be more command line friendly

--- README.md
+++ README.md
@@ -9,14 +9,26 @@ npm install minime -g
 #### Add minime.json to project
 ```javascript
 {
-    "jsRoot": "path/to/js",
-    "jsMap": {
-        "app.js" : [ "part1.js", "part2.js", ... ]
-    },
-    "cssRoot": "path/to/css",
-    "cssMap": {
-        "app.css" : [ "part1.css", "part2.css", ... ]
-    }
+    "js": [
+        {
+            "source": <path>,
+            "target": <path>,
+            "map": {
+                <targetFileName>: [<sourceFileName>, ...]
+            }
+        }, 
+        ...
+    ],
+    "css": [
+        {
+            "source": <path>,
+            "target": <path>,
+            "map": {
+                <targetFileName>: [<sourceFileName>, ...]
+            }
+        }, 
+        ...
+    ]
 }
 ```
 
--- index.js
+++ index.js
@@ -5,76 +5,105 @@ process.bin = process.title = 'minime';
 /*******************************************************************************
  * Imports
  ******************************************************************************/
+var program = require('commander');
+var fs = require('fs');
 var UglifyJS = require('uglify-js');
 var uglifycss = require('uglifycss');
-var fs = require('fs');
+var winston = require('winston');
+
+/*******************************************************************************
+ * Logging Level
+ ******************************************************************************/
+var logger = new (winston.Logger)({
+    transports: [
+        new (winston.transports.Console)()
+    ]
+});
+logger.level = 'info';
 
 /*******************************************************************************
  * Option Arguments
  ******************************************************************************/
-var option = process.argv[2];
-var options = {};
-if (option == "-d")
-    options = {
-        output : {
-            beautify : true
-        },
-        compress : false
-    };
+program
+    .usage('[options]')
+    .option('-f, --file [location]', 'minime.json location (if unspecified, will use current directory)', 'minime.json')
+    .option('-p, --production', 'only minified files will be built')
+    .parse(process.argv);
 
 /*******************************************************************************
  * Load minime.json
  ******************************************************************************/
 var minime;
-fs.readFile('minime.json', 'utf8', function (err,data) {
+fs.readFile(program.file, 'utf8', function (err,data) {
     if (err) {
-      return console.log('Unable to find minime.json! \n' + err);
+      return logger.info('Unable to find minime.json!\n' + err);
     }
     minime = JSON.parse(data);
+
+    if (program.production) logger.info('Building new assets in production mode...');
     
-    minifyJS(minime.jsRoot, minime.jsMap, options);
-    
-    minifyCSS(minime.cssRoot, minime.cssMap, {
-        maxLineLen : 0,
-        expandVars : true
-    });
+    logger.info('----------------------');
+    logger.info('*      js build      *');
+    logger.info('----------------------');
+    for (var i = 0; i < minime.js.length; i++) {
+        logger.info('Minifying js assets in ' + minime.js[i].source + '...');
+        minifyJS(minime.js[i], program.production);
+    }
+
+    logger.info('-----------------------');
+    logger.info('*      css build      *');
+    logger.info('-----------------------');
+    for (var i = 0; i < minime.css.length; i++) {
+        logger.info('Minifying css assets in ' + minime.css[i].source + '...');
+        minifyCSS(minime.css[i], {
+            maxLineLen : 0,
+            expandVars : true
+        });
+    }    
 });
 
 /*******************************************************************************
  * Script Functions
  ******************************************************************************/
-function minifyJS(jsRoot, jsMap, options) {
-    for (var target in jsMap) {
-        var input = buildJSLocation(jsRoot, jsMap[target]);
-        var result = UglifyJS.minify(input, options);
-        fs.writeFileSync(buildJSLocation(jsRoot, target), result.code);
-        console.log(target + ' created.');
+function minifyJS(js, p) {
+    for (var target in js.map) {
+        var input = buildPath(js.source, js.map[target]);        
+        var result;        
+        
+        // if production is not enabled, create a beautified version of the code
+        if (!p) {
+            result = UglifyJS.minify(input, {
+                output : {
+                    beautify : true
+                },
+                compress : false
+            });            
+        }
+        else {
+            result = UglifyJS.minify(input, {});
+        }
+        fs.writeFileSync(buildPath(js.target, target), result.code);
+        
+        logger.info(target + ' created.');
     }
+    logger.info('File(s) are available in ' + js.target + '.');
 }
 
-function minifyCSS(cssRoot, cssMap, options) {
-    for (var target in cssMap) {
-        var css = uglifycss.processFiles(buildCSSLocation(cssRoot, cssMap[target]),
+function minifyCSS(css, options) {
+    for (var target in css.map) {
+        var result = uglifycss.processFiles(buildPath(css.source, css.map[target]),
                 options);
-        fs.writeFileSync(buildJSLocation(cssRoot, target), css);
-        console.log(target + ' created.');
+        fs.writeFileSync(buildPath(css.source, target), result);
+        logger.info(target + ' created.');
     }
+    logger.info('File(s) are available in ' + css.target + '.');
 }
 
-function buildCSSLocation(cssRoot, files) {
+function buildPath(root, files) {
     if (files.constructor !== Array)
-        return cssRoot + "/" + files;
+        return root + "/" + files;
 
     for (var i = 0; i < files.length; i++)
-        files[i] = cssRoot + "/" + files[i];
-    return files;
-}
-
-function buildJSLocation(jsRoot, files) {
-    if (files.constructor !== Array)
-        return jsRoot + "/" + files;
-
-    for (var i = 0; i < files.length; i++)
-        files[i] = jsRoot + "/" + files[i];
+        files[i] = root + "/" + files[i];
     return files;
 }
