9342fca7042f2034c7b8fd9be7f5dac9f5758930
bob620 <bruder.kraft225@gmail.com>
2019-06-21 01:58:31 -0500

    fix(everything)

--- minime.js
+++ minime.js
@@ -5,20 +5,21 @@ process.bin = process.title = 'minime';
 /*******************************************************************************
  * Imports
  ******************************************************************************/
-var program = require('commander')
-  , fs = require('fs')
-  , mkdirp = require('mkdirp')
-  , uglifyjs = require('uglify-js')
-  , uglifycss = require('uglifycss')
-  , winston = require('winston');
+const fs = require('fs');
+
+const program = require('commander');
+const mkdirp = require('mkdirp');
+const uglifyjs = require('uglify-js');
+const uglifycss = require('uglifycss');
+const winston = require('winston');
 
 /*******************************************************************************
  * Logging Level
  ******************************************************************************/
-var logger = new (winston.Logger)({
-    transports: [
-        new (winston.transports.Console)()
-    ]
+let logger = new (winston.Logger)({
+	transports: [
+		new (winston.transports.Console)()
+	]
 });
 logger.level = 'info';
 
@@ -26,89 +27,94 @@ logger.level = 'info';
  * Option Arguments
  ******************************************************************************/
 program
-    .usage('[options]')
-    .option('-f, --file [location]', 'minime.json location (if unspecified, will use current directory)', 'minime.json')
-    .option('-r, --recursive', 'create all folders in path if they do not exist')
-    .option('-p, --production', 'js files will be minified (default: concat, uglify only)')
-    .parse(process.argv);
+.usage('[options]')
+.option('-f, --file [location]', 'minime.json location (if unspecified, will use current directory)', 'minime.json')
+.option('-r, --recursive', 'create all folders in path if they do not exist')
+.option('-p, --production', 'js files will be minified (default: concat, uglify only)')
+.parse(process.argv);
 
 /*******************************************************************************
  * Load minime.json
  ******************************************************************************/
-var minime;
-fs.readFile(program.file, 'utf8', function (err,data) {
-    if (err) {
-        return logger.info('Unable to find minime.json!\n' + err);
-    }
-    minime = JSON.parse(data);
+const configFile = program.file.endsWith('.json') ? program.file : program.file + '/minime.json';
+logger.info(`Looking for ${configFile}`);
 
-    if (program.production) logger.info('Building new assets in production mode...');
-    
-    logger.info('----------------------');
-    logger.info('*      js build      *');
-    logger.info('----------------------');
-    for (var i = 0; i < minime.js.length; i++) {
-        logger.info('Minifying js assets in ' + minime.js[i].source + '...');
-        minifyJS(minime.js[i]);
-    }
+fs.readFile(configFile, 'utf8', function (err, data) {
+	if (err)
+		logger.info('Unable to find minime.json!\n' + err);
+	else {
+		const minime = JSON.parse(data);
 
-    logger.info('-----------------------');
-    logger.info('*      css build      *');
-    logger.info('-----------------------');
-    for (var i = 0; i < minime.css.length; i++) {
-        logger.info('Minifying css assets in ' + minime.css[i].source + '...');
-        minifyCSS(minime.css[i], {
-            maxLineLen : 0,
-            expandVars : true
-        });
-    }    
+		if (program.production) logger.info('Building new assets in production mode...');
+
+		logger.info('----------------------');
+		logger.info('*      js build      *');
+		logger.info('----------------------');
+		if (minime.js.length !== undefined)
+			for (const jsSet of minime.js) {
+				logger.info(`Minifying js assets in ${jsSet.source}...`);
+				minifyJS(jsSet);
+			}
+		else
+			logger.info('Unabled to minify js, please make sure {js: []} exists');
+
+		logger.info('-----------------------');
+		logger.info('*      css build      *');
+		logger.info('-----------------------');
+		if (minime.js.length !== undefined)
+			for (const cssSet of minime.css) {
+				logger.info(`Minifying css assets in ${cssSet.source}...`);
+				minifyCSS(cssSet, {
+					maxLineLen: 0,
+					expandVars: true
+				});
+			}
+		else
+			logger.info('Unabled to minify css, please make sure {css: []} exists');
+	}
 });
 
 /*******************************************************************************
  * Script Functions
  ******************************************************************************/
-function minifyJS(js) {
-    for (var target in js.map) {
-        var input = buildPath(js.source, js.map[target]);        
-        var result;        
-        
-        // if production is not enabled, create a beautified version of the code
-        if (!program.production) {
-            result = uglifyjs.minify(input, {
-                output : {
-                    beautify : true
-                },
-                compress : false
-            });            
-        }
-        else {
-            result = uglifyjs.minify(input, {});
-        }
-        fs.writeFileSync(buildPath(js.target, target), result.code);
-        
-        logger.info(target + ' created.');
-    }
-    logger.info('File(s) are available in ' + js.target + '.');
+function minifyJS(jsSet) {
+	for (let target of Object.keys(jsSet.map)) {
+		let input = buildPath(jsSet.source, jsSet.map[target]);
+		let result;
+
+		// if production is not enabled, create a beautified version of the code
+		if (!program.production)
+			result = uglifyjs.minify(input, {
+				output: {
+					beautify: true
+				},
+				compress: false
+			});
+		else
+			result = uglifyjs.minify(input, {});
+
+		fs.writeFileSync(buildPath(jsSet.target, target), result.code);
+
+		logger.info(target + ' created.');
+	}
+	logger.info('File(s) are available in ' + jsSet.target + '.');
 }
 
-function minifyCSS(css, options) {
-    for (var target in css.map) {
-        var result = uglifycss.processFiles(buildPath(css.source, css.map[target]),
-                options);
-        fs.writeFileSync(buildPath(css.target, target), result);
-        logger.info(target + ' created.');
-    }
-    logger.info('File(s) are available in ' + css.target + '.');
+function minifyCSS(cssSet, options) {
+	for (let target of Object.keys(cssSet.map)) {
+		let result = uglifycss.processFiles(buildPath(cssSet.source, cssSet.map[target]), options);
+		fs.writeFileSync(buildPath(cssSet.target, target), result);
+		logger.info(target + ' created.');
+	}
+	logger.info('File(s) are available in ' + cssSet.target + '.');
 }
 
 function buildPath(root, files) {
-    if (program.recursive)
-      mkdirp.sync(root, {});
-  
-    if (files.constructor !== Array)
-        return root + "/" + files;
+	if (program.recursive)
+		mkdirp.sync(root, {});
 
-    for (var i = 0; i < files.length; i++)
-        files[i] = root + "/" + files[i];
-    return files;
+	if (typeof files === 'string')
+		return root + '/' + files;
+
+	return files.map(file => `${root}/${file}`);
 }
